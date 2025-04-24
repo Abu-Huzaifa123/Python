1a9a2114af60854beef7cc2758a3de83390b0e8d
Abu-Huzaifa123 <usmaniihuz21@gmail.com>
2025-04-25 00:07:58 +0530

    Update 04_problem4.py

--- Chapter 3 - PS/04_problem4.py
+++ Chapter 3 - PS/04_problem4.py
@@ -1,4 +1,6 @@
 name = "Harry is a good  boy and  "
 
-print(name.replace("  ", " "))
-print(name) # Strings are immutable which means that you cannot change them by running functions on them
+print(name.replace("  ", " ")) # it will print Harry is a good boy and means it is new string. so it updates original string by
+# replace function and create new string but old string will remain same.
+print(name) # Strings are immutable which means that you cannot change them by running functions on them. so it will
+ # print original string ie Harry is a good  boy and 
