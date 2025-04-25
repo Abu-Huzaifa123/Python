fbca2244ab595a0cde6a10f92d2e528dc7d11f5f
Abu-Huzaifa123 <usmaniihuz21@gmail.com>
2025-04-25 23:42:34 +0530

    Update 03_sets.py

--- Chapter 5/03_sets.py
+++ Chapter 5/03_sets.py
@@ -1,4 +1,4 @@
-e = set() # It will generate empty set. Dont use s = {} as it will create an empty dictionary.
+e = set() # It will generate empty set. Dont use s = {} as it will create an empty dictionary. As both starts with {} so there is difference between them(int ques: generate empty set)
 s = {1, 5, 32, 54,5, 5, 5} #exp of set
 
 
