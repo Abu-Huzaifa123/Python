971b70b64632e31c92ea2ef3ff632dc1a1143a09
Abu-Huzaifa123 <usmaniihuz21@gmail.com>
2025-04-25 23:47:27 +0530

    Update 03_sets.py

--- Chapter 5/03_sets.py
+++ Chapter 5/03_sets.py
@@ -1,5 +1,6 @@
 e = set() # It will generate empty set. Dont use s = {} as it will create an empty dictionary. As both starts with {} so there is difference between them(int ques: generate empty set)
 s = {1, 5, 32, 54,5, 5, 5} #exp of set
-
+# It will give output as { 32, 1, 5, 54} in set each values comes only once. So if we want to create something in which we want to
+# use something only once, we can use set.
 
 print(s)
