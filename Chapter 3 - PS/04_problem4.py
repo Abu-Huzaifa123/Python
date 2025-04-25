2ef38a34c89fc6bdd6a25f0cd07ca30df3035eae
Abu-Huzaifa123 <usmaniihuz21@gmail.com>
2025-04-25 22:19:32 +0530

    Update 04_problem4.py

--- Chapter 3 - PS/04_problem4.py
+++ Chapter 3 - PS/04_problem4.py
@@ -4,3 +4,5 @@ print(name.replace("  ", " ")) # it will print Harry is a good boy and means it 
 # replace function and create new string but old string will remain same.
 print(name) # Strings are immutable which means that you cannot change them by running functions on them. so it will
  # print original string ie Harry is a good  boy and 
+# If you update list then original list will be change whereas original string will remain same.
+# String usually starts with small letter whereas classes usually starts with capital letter.
