1becda410fc39956e9c1ea202c8fe1bf6941abe9
Abu-Huzaifa123 <usmaniihuz21@gmail.com>
2025-04-26 13:41:48 +0530

    Update 02_list_methods.py

--- Chapter 4/02_list_methods.py
+++ Chapter 4/02_list_methods.py
@@ -7,6 +7,6 @@ l1 = [1, 34,62, 2, 6, 11]
 # l1.sort()
 # l1.reverse()
 # l1.insert(2, 333333) # Insert 333333 such that its index in the list is 3
-value = l1.pop(3)
+value = l1.pop(3) #output=2
 print(value)
 print(l1)
